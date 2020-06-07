3a6eec78e83fff4dd58c66583bc01e5477043a64
sai1004 <saikiran.bavandla@gmail.com>
2020-06-07 20:51:39 +0530

    commit

--- src/app/pages/shopping-list/shopping-list.page.ts
+++ src/app/pages/shopping-list/shopping-list.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from "@angular/core";
 import { ShoppingService } from "../../services/shopping.service";
 import { ShoppingList } from "../../entities/ShoppingList";
 import { NgForm } from "@angular/forms";
-
+import { ActionSheetController, Platform } from "@ionic/angular";
 @Component({
   selector: "app-shopping-list",
   templateUrl: "./shopping-list.page.html",
@@ -14,7 +14,11 @@ export class ShoppingListPage implements OnInit {
   @Input()
   shoppingList: ShoppingList;
 
-  constructor(private _shoppingListService: ShoppingService) {
+  constructor(
+    public platform: Platform,
+    public actionSheetCtrl: ActionSheetController,
+    private _shoppingListService: ShoppingService
+  ) {
     this.shoppingList = new ShoppingList();
     this.savedList = _shoppingListService.getItems();
   }
@@ -27,5 +31,30 @@ export class ShoppingListPage implements OnInit {
 
   onRemove(index: number) {
     this._shoppingListService.removeItem(index);
+  }
+
+  async deleteActionSheet(index) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      cssClass: "action-sheets-basic-page",
+      buttons: [
+        {
+          text: "Delete",
+          role: "destructive",
+          icon: !this.platform.is("ios") ? "trash" : null,
+          handler: () => {
+            this.onRemove(index);
+          },
+        },
+        {
+          text: "Cancel",
+          role: "cancel", // will always sort to be on the bottom
+          icon: !this.platform.is("ios") ? "close" : null,
+          handler: () => {
+            console.log("Cancel clicked");
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
   }
 }
